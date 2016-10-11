3398ee6a347af6a821c49e4fd5bc667909a4d75b
Bartek Głowacki <marathone@gmail.com>
2016-10-11 21:26:03 +0200

    Third test passing

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,7 +2,11 @@ class Bottles
   def verse(number)
     <<-VERSE
 #{number} bottles of beer on the wall, #{number} bottles of beer.
-Take one down and pass it around, #{number - 1} bottles of beer on the wall.
+Take one down and pass it around, #{number - 1} #{plural?(number - 1) ? "bottles" : "bottle"} of beer on the wall.
     VERSE
+  end
+
+  def plural?(number)
+    number > 1
   end
 end
