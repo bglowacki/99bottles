d2921f9b3b221ba61604b8340b377c9f386b03c6
Bartek Głowacki <marathone@gmail.com>
2016-10-11 22:15:51 +0200

    Get Line objects outside

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,32 +1,33 @@
+class FirstLine
+  def initialize(number)
+    @number = number
+  end
+
+  def to_s
+    "#{@number} #{plural?(@number) ? "bottles" : "bottle"} of beer on the wall, #{@number} #{plural?(@number) ? "bottles" : "bottle"} of beer.\n"
+  end
+
+  def plural?(number)
+    number > 1
+  end
+end
+
+class SecondLine
+  def initialize(number)
+    @number = number - 1
+  end
+
+  def to_s
+    "Take one down and pass it around, #{@number} #{plural?(@number) ? "bottles" : "bottle"} of beer on the wall.\n"
+  end
+
+  def plural?(number)
+    number > 1
+  end
+end
+
+
 class Bottles
-  class FirstLine
-    def initialize(number)
-      @number = number
-    end
-
-    def to_s
-      "#{@number} #{plural?(@number) ? "bottles" : "bottle"} of beer on the wall, #{@number} #{plural?(@number) ? "bottles" : "bottle"} of beer.\n"
-    end
-
-    def plural?(number)
-      number > 1
-    end
-  end
-
-  class SecondLine
-    def initialize(number)
-      @number = number - 1
-    end
-
-    def to_s
-      "Take one down and pass it around, #{@number} #{plural?(@number) ? "bottles" : "bottle"} of beer on the wall.\n"
-    end
-
-    def plural?(number)
-      number > 1
-    end
-  end
-
   def verse(number)
     [FirstLine.new(number).to_s, SecondLine.new(number).to_s].join
   end
