156d5281087871c9316bdccffd136cb64d62bd66
Bartek Głowacki <marathone@gmail.com>
2016-10-11 22:06:20 +0200

    Split lines into methods

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,12 +1,17 @@
 class Bottles
   def verse(number)
-    <<-VERSE
-#{number} bottles of beer on the wall, #{number} bottles of beer.
-Take one down and pass it around, #{number - 1} #{plural?(number - 1) ? "bottles" : "bottle"} of beer on the wall.
-    VERSE
+    [first_line(number), second_line(number-1)].join
   end
 
   def plural?(number)
     number > 1
+  end
+
+  def first_line(number)
+    "#{number} bottles of beer on the wall, #{number} bottles of beer.\n"
+  end
+
+  def second_line(number)
+    "Take one down and pass it around, #{number} #{plural?(number) ? "bottles" : "bottle"} of beer on the wall.\n"
   end
 end
